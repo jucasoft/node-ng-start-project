01cf246380571b1d0d33122db83c7d97a959cdcd
jucasoft <juca@jucasoft.com>
2022-11-25 17:32:50 +0100

    4 files modified

--- ng14/src/app/app.module.ts
+++ ng14/src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThemeJngModule } from './core/theme/theme-jng.module';
 import { RootStoreModule } from './root-store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    RootStoreModule
+    RootStoreModule,
+    ThemeJngModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
